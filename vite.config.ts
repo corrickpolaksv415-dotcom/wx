ea4409c2f7f33a3d896150dd92b30147d201ff19
corrickpolaksv415-dotcom <corrickpolaksv415@gmail.com>
2026-04-01 21:52:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
   return {
+    base: 'wx', // 这里换成你的 GitHub 仓库名
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
